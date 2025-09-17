cca34f8c68db519d27e583dc1eacbf38a456ee55
Vvxzv <x1226466650@outlook.com>
2025-09-17 21:23:27 +0800

    1.1.2

--- src/main/java/net/vvxzv/farmerstfc/farmersTFC.java
+++ src/main/java/net/vvxzv/farmerstfc/farmersTFC.java
@@ -1,26 +1,17 @@
 package net.vvxzv.farmerstfc;
 
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.vvxzv.farmerstfc.common.registry.block;
 import net.vvxzv.farmerstfc.common.registry.creativeTAB;
 import net.vvxzv.farmerstfc.common.registry.item;
-import org.slf4j.Logger;
 
 
 @Mod(farmersTFC.MOD_ID)
 public class farmersTFC {
     public static final String MOD_ID = "farmerstfc";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public farmersTFC() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
