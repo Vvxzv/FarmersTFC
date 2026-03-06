16c4a9911bb0307b95ebf1ed891d7e9e3235e565
Vvxzv <x1226466650@outlook.com>
2026-03-06 16:47:08 +0800

    1.4.0

--- src/main/java/net/vvxzv/farmerstfc/FarmersTFC.java
+++ src/main/java/net/vvxzv/farmerstfc/FarmersTFC.java
@@ -1,80 +1,35 @@
 package net.vvxzv.farmerstfc;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.vvxzv.farmerstfc.common.registry.CreativeTAB;
 import net.vvxzv.farmerstfc.common.registry.FBlock;
 import net.vvxzv.farmerstfc.common.registry.FBlockEntity;
 import net.vvxzv.farmerstfc.common.registry.FItem;
 import org.slf4j.Logger;
+import vectorwing.farmersdelight.common.registry.ModCreativeTabs;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(FarmersTFC.MODID)
 public class FarmersTFC {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "farmerstfc";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public FarmersTFC(IEventBus modEventBus, ModContainer modContainer) {
         LOGGER.info("Initializing Farmers TFC");
-        // Register the commonSetup method for modloading
-
-        modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (FarmersTFC) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
 
         FBlock.BLOCKS.register(modEventBus);
         FItem.ITEMS.register(modEventBus);
-        CreativeTAB.CREATIVE_MODE_TAB.register(modEventBus);
         FBlockEntity.BLOCK_ENTITIES.register(modEventBus);
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+        modEventBus.addListener(this::addCreativeTab);
 
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-    }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @SuppressWarnings("removal")
-    @EventBusSubscriber(modid = MODID,  bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    private void addCreativeTab(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTab() == ModCreativeTabs.TAB_FARMERS_DELIGHT.get()){
+            event.accept(FBlock.PAN.get());
         }
     }
 }
