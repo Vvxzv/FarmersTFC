02eaca7e429a0a7acaf7b67f916c0833cf31005f
Vvxzv <x1226466650@outlook.com>
2026-08-08 17:07:40 +0800

    2.1.6

---
+++ src/main/java/net/vvxzv/farmerstfc/mixin/block/entity/CookingPotBlockEntityMixin.java
@@ -0,0 +1,22 @@
+package net.vvxzv.farmerstfc.mixin.block.entity;
+
+import net.dries007.tfc.common.capabilities.food.FoodCapability;
+import net.dries007.tfc.common.capabilities.food.IFood;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import vectorwing.farmersdelight.common.block.entity.CookingPotBlockEntity;
+
+@Mixin(CookingPotBlockEntity.class)
+public class CookingPotBlockEntityMixin {
+
+    @Inject(method = "isContainerValid", at = @At("HEAD"), cancellable = true, remap = false)
+    private void isContainerValid(ItemStack containerItem, CallbackInfoReturnable<Boolean> cir) {
+        IFood food = FoodCapability.get(containerItem);
+        if(food != null && food.isRotten()) {
+            cir.setReturnValue(false);
+        }
+    }
+}
